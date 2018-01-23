1eda883113df452c54a0cf5b3656eee8884c84b6
Bastien Duplessier <bastien.duplessier@gmail.com>
2018-01-24 00:27:06 +0100

    Add function in Eole module to pilot server

--- apps/eole/lib/eole.ex
+++ apps/eole/lib/eole.ex
@@ -15,4 +15,18 @@ defmodule Eole do
   def hello do
     :world
   end
+
+  @doc """
+  Create a new debit and add it to the corresponding account
+
+  ## Examples
+
+      iex> Eole.debit(%{iban: "GB87BARC20658244971655", amount: 1000
+      :ok
+
+  """
+  def debit(%{iban: _iban, amount: _amount} = params) do
+    Eole.Server.debit(params)
+  end
+
 end
