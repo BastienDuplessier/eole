c20065529fc8ecfbfb963b1cb130bead150a1e02
Bastien Duplessier <bastien.duplessier@gmail.com>
2018-01-24 20:44:56 +0100

    Document Eole.Account module

--- apps/eole/lib/eole/account.ex
+++ apps/eole/lib/eole/account.ex
@@ -1,4 +1,8 @@
 defmodule Eole.Account do
+  @moduledoc """
+  Documentation for Eole.Account.
+  """
+
   alias Eole.Debit
 
   # Time manipulation
@@ -7,17 +11,28 @@ defmodule Eole.Account do
   # Log
   require Logger
 
+  @doc """
+  Create a new debit, and insert into account in accounts list
+
+  """
   def debit(accounts, params) do
     iban = params[:iban]
     account = accounts |> Map.get(iban, []) |> update(params) |> check(iban)
     accounts |> Map.merge(%{iban => account})
   end
 
+  @doc """
+  Update account with new debit
+
+  """
   def update(account, params) do
     [Debit.build(params) | account]
   end
 
-  # %{} |> Eole.Account.debit(%{amount: 30, iban: "BAE"})
+  @doc """
+  Check if account had more than 10k€ of debit last 20 minutes. If true, output a warning log
+
+  """
   def check(account, iban) do
     ago = twenty_min_ago()
     account
@@ -26,11 +41,19 @@ defmodule Eole.Account do
     |> warn(account, iban)
   end
 
+  @doc """
+  Returns Kronow entity of 20 minutes ago
+
+  """
   def twenty_min_ago do
     use Kronos.Infix
     Kronos.now - ~t(20)minute
   end
 
+  @doc """
+  Outputs a warning if amount is over 10k€ and returns account everytime
+
+  """
   def warn(amount, account, iban) do
     if amount > 1_000_000 do # 10 000€ as euro-cents
       Logger.warn fn ->
